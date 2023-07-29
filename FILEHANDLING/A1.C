9e3866c54bddbd5b2fedea3aeac63990e31df53c
bharatdangat <139533258+bharatdangat@users.noreply.github.com>
2023-07-29 17:29:15 +0530

    Create A1.C

---
+++ FILEHANDLING/A1.C
@@ -0,0 +1,47 @@
+//SEARCH GIVEN NUMBER FROM ''nunbers.txt''FILE USING BINARY SEARCH.
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+  
+  int a[100],x,num,top,bottom,mid,flag=0,n;
+  FILE *fp1;      //declare
+  fp1=fopen("sorted numbers.txt","r"); //open
+  if(fp1==NULL)
+  {
+   printf("file is not found");
+   exit(0);                         //x=accept no 
+  }                                 //to search
+                                    //num=from file 
+   printf("Enter number to search");
+   scanf("%d",&x");
+  while(!feof(fp1))
+  {
+    fscanf(fp1,"%d",&num);
+    a[i++]=num;
+  }
+  i=n;
+    top=0;
+    bottom=n-1;
+    while(top<=bottom)
+    {
+      mid=(top+bottom)/2;
+       if(a[mid]==x)
+       {
+        flag=1;
+        break;
+       }
+       if(x>a[mid])
+       {
+       top=mid+1;
+       else
+        bottom=mid-1;
+       }
+    }
+     if(flag=1)
+     printf("number is found");
+     else
+     printf("number is not found");
+  }
+  fclose(fp1);
+}
